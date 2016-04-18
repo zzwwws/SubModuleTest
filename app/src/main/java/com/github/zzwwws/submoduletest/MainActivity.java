238d9c40a7deeb4bd04a06cd9658e3a8b0db3865
zengzhiwen <zhiwen.zeng@gmail.com>
2016-04-18 20:13:02 +0800

    add submodule

--- app/src/main/java/com/github/zzwwws/submoduletest/MainActivity.java
+++ app/src/main/java/com/github/zzwwws/submoduletest/MainActivity.java
@@ -3,11 +3,14 @@ package com.github.zzwwws.submoduletest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.zzwwws.sublibrary.MethodTest;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MethodTest.A();
     }
 }
